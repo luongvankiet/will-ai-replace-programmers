68b2bbf5a5e1f0d8787589caa585e69c6365ebd8
luongvankiet <39187268+luongvankiet@users.noreply.github.com>
2025-04-20 21:43:56 +1000

    update: package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/py-presentations/will-ai-replace-programmers/",
+  base: "/will-ai-replace-programmers",
 });
